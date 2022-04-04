8a08785211c32b0468ecbe5411fc13e532f4f86a
hogan3c1 <hogan3c1@gmail.com>
2022-04-04 22:46:42 +0800

    first commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function add (num1, num2) {
+    return num1 + num2
+}
+
+function subtract (num1, num2) {
+    return num1 - num2
+}
+
+// module.exports.add = add;
+// module.exports.subtract = subtract;
+
+// exports.add = add;
+// exports.subtract = subtract;
+
+module.exports = {
+    add,
+    subtract,
+}
